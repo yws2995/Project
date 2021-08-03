e38be5904b7249a98526f512e6389cf54579692a
yws2995 <yws2995@naver.com>
2021-08-03 17:55:32 +0900

    web.js

--- FrontEnd/ChromeWeb/app.js
+++ FrontEnd/ChromeWeb/app.js
@@ -1,24 +1,28 @@
-// const longinForm = document.getElementById("login-form");
-// const loginInput = longinForm.querySelector("input"); 
-// const loginButton = longinForm.querySelector("button"); 
-
-const loginForm = document.querySelector("#login-form")
-const loginInput = document.querySelector("#login-form input"); 
-const link = document.querySelector("a");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add("HIDDEN_CLASSNAME");
-    const username = loginInput.value;
-    greeting.innerText = "Hello " + username;
-    greeting.classList.remove("HIDDEN_CLASSNAME")
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
-function handleLinkClick(event) {
-    event.preventDefault();
-}
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
